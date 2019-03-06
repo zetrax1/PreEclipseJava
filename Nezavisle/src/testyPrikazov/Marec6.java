293d8c32f87851de951fc223b8f4d4bd30a0603a
zetrax <zetrax@zetrax-Lenovo-ideapad-310-15ISK>
2019-03-06 18:14:52 +0100

    First commit  ... start with github , learning new information about using github in eclipse oxigen

---
+++ Nezavisle/src/testyPrikazov/Marec6.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package testyPrikazov;
+
+/**
+ * @author zetrax
+ *
+ */
+public class Marec6 {
+	
+	
+	
+	
+	
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
